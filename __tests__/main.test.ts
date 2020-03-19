aac3969642111617819d9865213d8e3b35d6a027
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2020-03-19 15:54:52 -0400

    fix: remove unused js files

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,11 +1,12 @@
 import * as core from '@actions/core'
-import { run } from '../src/main'
+import {run} from '../src/main'
 
 jest.mock('@actions/core')
 
 describe('when running the action', () => {
-
-  const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput >
+  const fakeSetOutput = core.setOutput as jest.MockedFunction<
+    typeof core.setOutput
+  >
 
   it('should output something', async () => {
     await run()
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import * as core from '@actions/core'
 export async function run(): Promise<void> {
   try {
     core.setOutput('result', 'dummy output')
-
   } catch (error) {
     core.setFailed(error.message)
   }
