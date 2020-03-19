ff1f200dc2b26f4bfba11e3f6abd16c13d54e8e2
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2020-03-19 15:13:09 -0400

    feat: bare bones GitHub Action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.setOutput('result', 'dummy output')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
