6347a61df026b51282fe1b38a801d9de429fcff3
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2020-03-20 18:00:27 -0400

    feat: generate a dummy HTTP request and status output

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import * as core from '@actions/core'
+import axios from 'axios'
 
 export async function run(): Promise<void> {
   try {
     const url: string = core.getInput('url')
     const query: string = core.getInput('query', {required: true})
 
-    core.info(`url:\n${url}`)
+    core.info(`url: ${url}`)
     core.info(`query:\n${query}`)
 
-    core.setOutput('result', 'dummy output')
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos/1'
+    )
+    const data = response.data as Object
+
+    core.info(`response status: ${response.status}`)
+    core.info(`response body:\n${JSON.stringify(data)}`)
+
+    core.setOutput('status', `${response.status}`)
+    core.setOutput('result', `${JSON.stringify(data)}`)
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
