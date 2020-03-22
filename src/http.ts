0fe761fbbd0cb20b885febb9ad0413d6312f8dc5
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2020-03-22 09:49:06 -0400

    feat: implement http.request http.get http.post

--- src/http.ts
+++ src/http.ts
@@ -1,9 +1,34 @@
 import axios from 'axios'
 
-export async function post(
+export async function request(
   url: string,
-  payload: string
+  method: Method,
+  data: string
 ): Promise<[number, Object]> {
+  switch (method.toUpperCase()) {
+    case 'POST':
+      return post(url, data)
+    case 'GET':
+      return get(url)
+
+    default:
+      throw new Error(`unimplemented HTTP method: ${method}`)
+  }
+}
+
+async function get(url: string): Promise<[number, Object]> {
+  const response = await axios.get(url, {
+    responseType: 'json',
+    headers: {'Content-Type': 'application/json'}
+  })
+
+  const status = response.status
+  const data = response.data as Object
+
+  return [status, data]
+}
+
+async function post(url: string, payload: string): Promise<[number, Object]> {
   const response = await axios.post(url, payload, {
     responseType: 'json',
     headers: {'Content-Type': 'application/json'}
@@ -14,3 +39,23 @@ export async function post(
 
   return [status, data]
 }
+
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+  | 'link'
+  | 'LINK'
+  | 'unlink'
+  | 'UNLINK'
