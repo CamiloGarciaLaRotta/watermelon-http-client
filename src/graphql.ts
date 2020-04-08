8889aa20d515bc919f2ffe5d822fd9d5931f57c6
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2020-04-07 21:17:27 -0400

    feat: encode GraphQL variables in payload

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,7 +1,3 @@
-type Operation = 'query' | 'mutation'
-
-const MUTATION_OP: Operation = 'mutation'
-
 /**
  * Returns the stringified JSON payload which corresponds to the GraphQL operation.
  * If no operation is explicitly defined, it will default to `query`.
@@ -9,10 +5,17 @@ const MUTATION_OP: Operation = 'mutation'
  * @param data - the raw HTTP request payload
  * @returns `{"query": data}` or `{"mutation": data}`
  */
-export function graphqlPayloadFor(data: string): string {
-  if (data.startsWith(MUTATION_OP)) {
-    return JSON.stringify({mutation: data})
-  }
-
-  return JSON.stringify({query: data})
+export function graphqlPayloadFor(
+  rawData: string,
+  rawVariables: string
+): string {
+  const data = minify(rawData)
+  const variables = minify(rawVariables)
+  return JSON.stringify({query: data, variables: JSON.parse(variables)})
 }
+
+/**
+ * Returns the minified version of the string.
+ * e.g. no line breaks and single spaces between each word
+ */
+const minify = (s: string): string => s.replace(/\s+/g, ' ').trim()
